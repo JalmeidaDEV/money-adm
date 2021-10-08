8d2d44de2b284037e3c7fd2342941a0619922e45
Jacó Almeida <jacojsa06@gmail.com>
2021-10-08 00:28:22 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import * as C from './app.styles'
+
+
+const App = () => {
+    return (
+      <C.Container>
+        <C.Header>
+          <C.HeaderText>Sistema Financeiro</C.HeaderText>
+        </C.Header>
+        <C.Body>
+          ...
+        </C.Body>
+      </C.Container>
+
+    )
+
+}
+
+export default App; 
